be179165b3fe748f2cf346c98db14ced5957072e
Subramanyam Raju <subbu.balaraju@gmail.com>
2021-01-23 19:34:45 +0530

    - Added fragment demo

---
+++ XNativeAndroid/Views/FragmentDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace XNativeAndroid.Views
+{
+    public class FragmentDemo : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            try
+            {
+                SetContentView(Resource.Layout.fragemnt_demo_main);
+            }
+            catch (Exception ex)
+            {
+                Log.Debug("MainActivity", ex.Message);
+            }
+        }
+    }
+}
---
+++ XNativeAndroid/Views/FragmentDemoDetails.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using XNativeAndroid.Fragements;
+
+namespace XNativeAndroid.Views
+{
+    [Activity(Label = "Details Activity", Theme = "@style/Theme.AppCompat")]
+    public class FragmentDemoDetails : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var index = Intent.Extras.GetInt("current_play_id", 0);
+
+            var details = DetailsFragment.NewInstance(index); // Details
+            var fragmentTransaction = FragmentManager.BeginTransaction();
+            fragmentTransaction.Add(Android.Resource.Id.Content, details);
+            fragmentTransaction.Commit();
+        }
+    }
+}
