87b481fab8a1432f15d3bfbaf5ecbfd89f977a7d
Subramanyam Raju <subbu.balaraju@gmail.com>
2021-01-23 15:42:29 +0530

    - Added Tabbed Layout demo

---
+++ XNativeAndroid/Fragements/Fragments_Tabs.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace XNativeAndroid.Fragements
+{
+    // these fragemtns for tab layout demo
+    public class SpeakersFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragement_tab, null);
+            view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.speakers_tab_label);
+
+            var img = view.FindViewById<ImageView>(Resource.Id.imageView1);
+            img.SetImageResource(Resource.Drawable.ic_action_speakers);
+            var filter = new PorterDuffColorFilter(Color.Black, PorterDuff.Mode.SrcIn);
+            img.SetColorFilter(filter);
+
+            return view;
+        }
+    }
+
+    public class SessionsFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragement_tab, null);
+            view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.sessions_tab_label);
+
+            var img = view.FindViewById<ImageView>(Resource.Id.imageView1);
+            img.SetImageResource(Resource.Drawable.ic_action_sessions);
+            var filter = new PorterDuffColorFilter(Color.Black, PorterDuff.Mode.SrcIn);
+            img.SetColorFilter(filter);
+
+            return view;
+        }
+    }
+
+    public class WhatsOnFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragement_tab, null);
+            view.FindViewById<TextView>(Resource.Id.textView1).SetText(Resource.String.sessions_tab_label);
+
+            var img = view.FindViewById<ImageView>(Resource.Id.imageView1);
+            img.SetImageResource(Resource.Drawable.ic_action_whats_on);
+            var filter = new PorterDuffColorFilter(Color.Black, PorterDuff.Mode.SrcIn);
+            img.SetColorFilter(filter);
+
+            return view;
+        }
+    }
+}
---
+++ XNativeAndroid/Views/TabsDemo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using XNativeAndroid.Fragements;
+using V4Fragment = Android.Support.V4.App.Fragment;
+using V4FragmentManager = Android.Support.V4.App.FragmentManager;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+
+namespace XNativeAndroid.Views
+{
+    public class TabsDemo : AppCompatActivity
+    {
+
+        ViewPager viewpager;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.tabs_demo);
+
+            viewpager = FindViewById<Android.Support.V4.View.ViewPager>(Resource.Id.viewpager);
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+
+            SetSupportActionBar(toolbar);
+
+            //SupportActionBar.SetIcon(Resource.Drawable.ic_done);
+            SupportActionBar.SetTitle(Resource.String.tab_demo_label);
+
+            //SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            //SupportActionBar.SetDisplayShowTitleEnabled(false);
+            //SupportActionBar.SetHomeButtonEnabled(false);
+
+
+            setupViewPager(viewpager);
+
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += (sender, e) => {
+                Snackbar.Make(fab, "Here's a snackbar!", Snackbar.LengthLong).SetAction("Action",
+                    v => Console.WriteLine("Action handler")).Show();
+            };
+
+            var tabLayout = FindViewById<TabLayout>(Resource.Id.tabs);
+            tabLayout.SetupWithViewPager(viewpager);
+        }
+
+        void setupViewPager(Android.Support.V4.View.ViewPager viewPager)
+        {
+            var adapter = new Adapter(SupportFragmentManager);
+            adapter.AddFragment(new WhatsOnFragment(), GetString(Resource.String.whatson_tab_label));
+            adapter.AddFragment(new SpeakersFragment(), GetString(Resource.String.speakers_tab_label));
+            adapter.AddFragment(new SessionsFragment(), GetString(Resource.String.sessions_tab_label));
+
+            viewPager.Adapter = adapter;
+            viewpager.Adapter.NotifyDataSetChanged();
+            //viewpager.OffscreenPageLimit(4);
+        }
+
+    }
+
+    class Adapter : Android.Support.V4.App.FragmentPagerAdapter
+    {
+        List<V4Fragment> fragments = new List<V4Fragment>();
+        List<string> fragmentTitles = new List<string>();
+
+
+        public Adapter(V4FragmentManager fm) : base(fm)
+        {
+        }
+
+        public void AddFragment(V4Fragment fragment, String title)
+        {
+            fragments.Add(fragment);
+            fragmentTitles.Add(title);
+
+
+        }
+
+        public override V4Fragment GetItem(int position)
+        {
+            return fragments[position];
+
+        }
+
+        public override int Count
+        {
+            get { return fragments.Count; }
+        }
+
+        public override Java.Lang.ICharSequence GetPageTitleFormatted(int position)
+        {
+            return new Java.Lang.String(fragmentTitles[position]);
+        }
+
+
+    }
+
+}
